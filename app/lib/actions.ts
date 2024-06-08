7d6e6dc8e06ebb1d727e3541cb5329486a2c78a9
Salih Yılmaz <salih_yil@live.com>
2024-06-08 14:40:13 +0300

    Chapter 13 - Handling Errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -37,10 +37,16 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (error) {
+    return {
+      message: "Database Error: Failed to Create Invoice.",
+    };
+  }
 
   //revalidatePath allows you to purge cached data on-demand for a specific path.
   // Since you're updating the data displayed in the invoices route, you want to clear this cache and trigger a new request to the server.
@@ -57,17 +63,30 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch (error) {
+    return {
+      message: "Database Error: Failed to Update Invoice.",
+    };
+  }
+
+  //redirect'in try/catch bloğunun dışında nasıl çağrıldığına dikkat edin. Bunun nedeni, redirect'in catch bloğu tarafından yakalanacak bir hata atarak çalışmasıdır. Bundan kaçınmak için redirect'i try/catch'ten sonra çağırabilirsiniz. redirect'e yalnızca try başarılı olursa erişilebilir.
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath("/dashboard/invoices");
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices");
+    return { message: "Deleted Invoice." };
+  } catch (error) {
+    return { message: "Database Error: Failed to Delete Invoice." };
+  }
 }
